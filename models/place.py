8845944f46685fcc85fd53c4f7a3ca9ea4dbc695
oladakinjoye <oladakinjoye@gmail.com>
2024-02-09 14:32:48 -0800

    update 12

--- models/place.py
+++ models/place.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""Define the Place Class."""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class a place.
+    """Represent the attributes a place.
 
     Attributes:
         city_id (str): The City id.
-	user_id (str): The User id.
-	name (str): The Name of the Place.
-        description (str): The Description of the Place.
-        number_rooms (int): The number of Rooms of the Place.
-        number_bathrooms (int): The number of Bathrooms of the Place.
-        max_guest (int): The maximum number of Guests of the Place.
-        price_by_night (int): The price by night of the Place.
-        latitude (float): The latitude of the Place.
-        longitude (float): The longitude of the Place.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
         amenity_ids (list): A list of Amenity ids.
     """
-        city_id = ""
-	user_id = ""
-    	name = ""
-        description = ""
-    	number_rooms = 0
-    	number_bathrooms = 0
-    	max_guest = 0
-    	price_by_night = 0
-    	latitude = 0.0
-    	longitude = 0.0
-    	amenity_ids = []
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
